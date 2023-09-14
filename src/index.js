93c63f604a26d4242f6c525a3cea7a759dded9a4
Sean O'Grady <ogrady.sean.a@gmail.com>
2023-09-14 19:04:20 -0400

    Fetch dog images, fetch breed list, implement filter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
-console.log('%c HI', 'color: firebrick')
+console.log('%c HI', 'color: firebrick');
+fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(response => {return response.json()})
+    .then(dogImages => {
+        const dogImageContainer = document.getElementById("dog-image-container");
+        dogImages.message.forEach(dog => {
+            const newImage = document.createElement("img");
+            newImage.src = dog;
+            dogImageContainer.appendChild(newImage);
+        });
+    });
+fetch("https://dog.ceo/api/breeds/list/all")
+    .then(response => {return response.json()})
+    .then(dogBreeds => {
+        const dogBreedsContainer = document.getElementById("dog-breeds");
+        Object.keys(dogBreeds.message).forEach(breed => {
+            const newLi = document.createElement("li");
+            newLi.textContent = breed[0].toUpperCase() + breed.slice(1);
+            newLi.addEventListener('click', () => {
+                if (newLi.style.color !== 'blue') newLi.style.color = 'blue';
+                else newLi.style.color = '';
+            });
+            dogBreedsContainer.appendChild(newLi);
+        });
+    });
+const breedDropdown = document.getElementById("breed-dropdown");
+while (breedDropdown.firstChild) {
+    breedDropdown.removeChild(breedDropdown.lastChild);
+}
+const noFilter = document.createElement('option');
+noFilter.value = noFilter.textContent = '-';
+breedDropdown.appendChild(noFilter);
+
+for (let i = 65; i <= 90; i++) {
+    const letter = String.fromCharCode(i);
+    const option = document.createElement("option");
+    option.value = option.textContent = letter;
+    breedDropdown.appendChild(option);
+  }  
+breedDropdown.addEventListener('change', () => {
+    const breedList = document.querySelectorAll("ul li");
+    breedList.forEach(breed => {
+        if (breed.textContent[0] !== breedDropdown.value && breedDropdown.value !== "-") {
+            breed.style.display = "none";
+        } else {
+            breed.style.display = "list-item";
+        }
+    });
+});
